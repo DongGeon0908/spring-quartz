5b534f77da0a13d148e08e3b4ef2d1d896984f04
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2023-01-01 16:22:35 +0900

    gradle 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,12 +4,28 @@ plugins {
     id("org.springframework.boot") version "2.7.8-SNAPSHOT"
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
     kotlin("jvm") version "1.6.21"
+    kotlin("kapt") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
+    idea
 }
 
 group = "com.goofy"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+idea {
+    module {
+        val kaptMain = file("build/generated/source/kapt/main")
+        sourceDirs.add(kaptMain)
+        generatedSourceDirs.add(kaptMain)
+    }
+}
 
 repositories {
     mavenCentral()
@@ -17,10 +33,23 @@ repositories {
     maven { url = uri("https://repo.spring.io/snapshot") }
 }
 
+object DependencyVersion {
+    const val KOTLIN_LOGGING_VERSION = "3.0.0"
+}
+
 dependencies {
+    // spring starter
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-quartz")
+
+    // kotlin
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // log
+    implementation("io.github.microutils:kotlin-logging-jvm:${DependencyVersion.KOTLIN_LOGGING_VERSION}")
+
+    // test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
@@ -31,6 +60,18 @@ tasks.withType<KotlinCompile> {
     }
 }
 
+tasks.withType<Wrapper> {
+    gradleVersion = "7.5.1"
+}
+
 tasks.withType<Test> {
     useJUnitPlatform()
 }
+
+tasks.getByName<Jar>("jar") {
+    enabled = false
+}
+
+defaultTasks("bootRun")
+
+val Project.isSnapshotVersion: Boolean get() = version.toString().endsWith("SNAPSHOT")
