8cf487046935735d904a93d974626df98a709451
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2023-01-01 17:26:35 +0900

    fix: quarts management entity

--- src/main/kotlin/com/goofy/springquartz/entity/QuartsManagement.kt
+++ src/main/kotlin/com/goofy/springquartz/entity/QuartsManagement.kt
@@ -1,5 +1,6 @@
 package com.goofy.springquartz.entity
 
+import java.time.ZonedDateTime
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
@@ -14,5 +15,20 @@ class QuartsManagement(
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long = -1
+    val id: Long = -1,
+
+    @Column(name = "title")
+    val title: String,
+
+    @Column(name = "content")
+    val content: String,
+
+    @Column(name = "start_at")
+    val startAt: ZonedDateTime,
+
+    @Column(name = "end_at")
+    val endAt: ZonedDateTime,
+
+    @Column(name = "extra")
+    val extra: String? = null
 ) : BaseEntity()
